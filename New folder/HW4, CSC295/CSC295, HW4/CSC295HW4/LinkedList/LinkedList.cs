48f958993b677667eb8a2e7ad4c9b21202c66185
JonathanDLee1998 <Jonathan.David.Lee@outlook.com>
2024-07-29 21:04:21 -0700

    blarp

---
+++ New folder/HW4, CSC295/CSC295, HW4/CSC295HW4/LinkedList/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC295HW4.LinkedList
+{
+    public class LinkedList
+    {
+        public Node? First { get; set; }
+
+        public void InsertFirst(int data)
+        {
+            
+            Node newNode = new Node();
+
+            
+            newNode.Data = data;
+
+            
+            newNode.Next = First;
+
+            
+            First = newNode;
+        }
+
+        public Node DeleteFirst(int data)
+        {
+            if (First == null)
+            {
+                throw new InvalidOperationException("Cannot delete from an empty list.");
+            }
+            Node temp = First;
+            First = First.Next;
+            return temp;
+        }
+
+        public void DisplayList()
+        {
+            Console.WriteLine("Iterating through list...");
+            Node? current = First;
+            while (current != null) 
+            {
+                current.DisplayNode();
+                current = current.Next;
+            }
+        }
+
+        public void InsertLast(int data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+
+            if (First == null)
+            {
+                
+                First = newNode;
+            }
+            else
+            {
+                Node current = First;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                
+                current.Next = newNode;
+            }
+        }
+
+    }
+}
